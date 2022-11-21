766eb1c47c6a3809a6c9831fa5b1e9ec54e3ce31
AbsoluteAsk <absoluteasklad@gmail.com>
2022-11-21 12:00:05 +0530

    DependencyInjection_MongoDb Connection

---
+++ src/Infrastructure/Odyssey.DiagnosticCertificateService.Bootstrap/DependencyInjector.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using System;
+
+namespace Odyssey.DiagnosticCertificateService.Bootstrap
+{
+    public class DependencyInjector
+    {
+        public static IServiceCollection ConfigureServices(IServiceCollection services,IConfiguration configuration)
+        {
+            MongoDb(services, configuration);
+            return services;
+
+        }
+
+        private static void MongoDb(IServiceCollection services, IConfiguration configuration)
+        {
+            var mongoClient = new MongoClient(configuration.GetSection("CMSDatabaseSettings:ConnectionString").Value);
+            var mongoDatabase = new MongoClient(configuration.GetSection("CMSDatabaseSettings:CertificateManagementService").Value);
+
+           
+
+            throw new NotImplementedException();
+        }
+    }
+}
